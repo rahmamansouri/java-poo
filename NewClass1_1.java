028d2d09d62aad3b95f58a4a8683615f1d71b4d8
rahmamansouri <rahma.mansouri@etudiant-fst.utm.tn>
2020-12-31 18:30:38 +0100

    second commit

---
+++ NewClass1_1.java
@@ -0,0 +1,13 @@
+class NewClass1_1 {
+  public static void main(String[] args) {
+
+    int number = -10;
+
+    // checks if number is greater than 0
+    if (number > 0) {
+      System.out.println("The number is positive.");
+    }
+
+    System.out.println("Statement outside if block");
+  }
+}
